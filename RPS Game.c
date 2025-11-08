fd21e1156c29362916fd4f966709e643e78e9fea
Fabio Bustamante Romillo <f.bustamanteromillo@gmail.com>
2025-11-07 19:54:23 -0500

    Implemented minimal rcsGame code to pass Player1Wins test (Green stage)

---
+++ RPS Game.c
@@ -0,0 +1,8 @@
+#include "rpcGame.h"
+#include <string.h>
+
+char* rcsGame(char player1[], char player2[]) {
+	if ((strcmp(player1, "rock") == 0 && strcmp(player2, "scissors") == 0) || (strcmp(player1, "scissors") == 0 && strcmp(player2, "paper") == 0) || (strcmp(player1, "paper") == 0 && strcmp(player2, "rock") == 0)) {
+		return "Player 1";
+	}
+}
---
+++ rpcGame.h
@@ -0,0 +1,9 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	char* rcsGame(char player1[], char player2[]);
+
+#ifdef __cplusplus
+}
+#endif
