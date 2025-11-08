63ee629548d373c60e35fff1f74e04ec6ce92cde
Fabio Bustamante Romillo <f.bustamanteromillo@gmail.com>
2025-11-07 20:27:41 -0500

    Fixed rcsGame.c  so it passes all tests and added another test method for "Player2Wins" (Blue Stage)

--- RPS Game Tests/RPS Game Tests.cpp
+++ RPS Game Tests/RPS Game Tests.cpp
@@ -16,17 +16,23 @@ namespace RPSGameTests
 			char player2[] = "scissors";
 			Assert::AreEqual("Player 1", rcsGame(player1, player2));
 		}
+		TEST_METHOD(Player2Wins)
+		{
+			char player1[] = "paper";
+			char player2[] = "scissors";
+			Assert::AreEqual("Player 2", rcsGame(player1, player2));
+		}
 		TEST_METHOD(Draw)
 		{
 			char player1[] = "paper";
 			char player2[] = "paper";
-			Assert::AreEqual("Player 1", rcsGame(player1, player2));
+			Assert::AreEqual("Draw", rcsGame(player1, player2));
 		}
 		TEST_METHOD(Invalid)
 		{
 			char player1[] = "rock";
-			char player2[] = "2";
-			Assert::AreEqual("Player 1", rcsGame(player1, player2));
+			char player2[] = "s";
+			Assert::AreEqual("Invalid", rcsGame(player1, player2));
 		}
 	};
 }
--- RPS Game.c
+++ RPS Game.c
@@ -1,8 +1,16 @@
 #include "rpcGame.h"
 #include <string.h>
 
+
 char* rcsGame(char player1[], char player2[]) {
+	if (strcmp(player1, "rock") != 0 && strcmp(player1, "paper") != 0 && strcmp(player1, "scissors") != 0 || strcmp(player2, "rock") != 0 && strcmp(player2, "paper") != 0 && strcmp(player2, "scissors") != 0) {
+		return "Invalid";
+	}
+	if (strcmp(player1, player2) == 0) {
+		return "Draw";
+	}
 	if ((strcmp(player1, "rock") == 0 && strcmp(player2, "scissors") == 0) || (strcmp(player1, "scissors") == 0 && strcmp(player2, "paper") == 0) || (strcmp(player1, "paper") == 0 && strcmp(player2, "rock") == 0)) {
 		return "Player 1";
 	}
+	return "Player 2";
 }
